2b3aab6898fd2a901de5e02251d8087161c5059e
yuichi.kuwahara <yuichi.kuwahara.0@gmail.com>
2015-10-17 11:13:52 +0900

    新規作成

---
+++ src/main/java/org/sitoolkit/ad/mw/App.java
@@ -0,0 +1,15 @@
+package org.sitoolkit.ad.mw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("      Hello SIToolkit!");
+        System.out.println("");
+        System.out.println("");
+    }
+}
