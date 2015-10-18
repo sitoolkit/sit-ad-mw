e6c0d3e97d0b52eaffdfd478a06c0cceec585b9e
yuichi.kuwahara <yuichi.kuwahara.0@gmail.com>
2015-10-18 14:58:29 +0900

    動作イメージを実装

--- src/main/java/org/sitoolkit/ad/mw/App.java
+++ src/main/java/org/sitoolkit/ad/mw/App.java
@@ -1,15 +1,22 @@
 package org.sitoolkit.ad.mw;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("      Hello SIToolkit!");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("インストールするミドルウェアを選択してください。");
+        System.out.println("1:Application Server");
+        System.out.println("2:Database Management System");
+        System.out.print("?> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String in = scanner.nextLine();
+
+        System.out.println(in + "をインストールします。");
+        scanner.close();
     }
 }
